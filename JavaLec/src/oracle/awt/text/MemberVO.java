247f86fc9563e75bd392a720485ce683e84324a0
SYU <SYU@DESKTOP-1VHBQ5H>
2025-01-17 16:24:18 +0900

    AWT & DB connection ex

---
+++ JavaLec/src/oracle/awt/text/MemberVO.java
@@ -0,0 +1,23 @@
+package oracle.awt.text;
+
+public class MemberVO {
+	private String id;
+	private String pwd;
+
+	public MemberVO() {
+
+	}
+
+	public MemberVO(String id, String pwd) {
+		this.id = id;
+		this.pwd = pwd;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public String getPwd() {
+		return pwd;
+	}
+}
